e0fe21c5a630750692f3b406a65c52a76c4aac8d
RayanR31 <rayan-31100@hotmail.com>
2024-02-21 18:25:51 +0100

    ToolsBoids

---
+++ Assets/Dev/Scripts/Ennemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemy : MonoBehaviour
+{
+    /// <summary>
+    /// Code non prêt pour faire un vrai prédateur
+    /// </summary>
+    public float forceX; 
+    public float forceY; 
+    public float forceZ; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3 (forceX, forceY, forceZ) * Time.deltaTime;
+    }
+}
